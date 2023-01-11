bbb9ed885846c235cddea4880f1ca8d33caf27db
kche1106 <kche011106@gmail.com>
2023-01-11 13:37:52 +0900

    init setting

---
+++ src/main/java/kr/co/FreeAndPre/FreeAndPreApplication.java
@@ -0,0 +1,13 @@
+package kr.co.FreeAndPre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeAndPreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeAndPreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/FreeAndPre/FreeAndPreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.FreeAndPre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeAndPreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
