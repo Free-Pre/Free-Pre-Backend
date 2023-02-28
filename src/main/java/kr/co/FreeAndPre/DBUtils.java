5ab9b54a8612d4069defb27f4e8697e246c44c1e
kche1106 <kche011106@gmail.com>
2023-02-28 11:59:11 +0900

    connect localhost

--- src/main/java/kr/co/FreeAndPre/DBUtils.java
+++ src/main/java/kr/co/FreeAndPre/DBUtils.java
@@ -9,11 +9,13 @@ public class DBUtils {
 
         String driver = "com.mysql.cj.jdbc.Driver";
 
-        String url = "jdbc:mysql://34.64.190.83:3306/FreePreDB?useSSL=false&useUnicode=true&serverTimezone=Asia/Seoul";
+//        String url = "jdbc:mysql://34.64.190.83:3306/FreePreDB?useSSL=false&useUnicode=true&serverTimezone=Asia/Seoul";
+        String url = "jdbc:mysql://127.0.0.1:3306/FreePreDB";
 
         String user = "root"; // 데이터 베이스 사용자의 접속 아이디 입력
 
-        String password = "root!"; // 데이터 베이스 사용자의 접속 비밀번호 입력
+//        String password = "root!"; // 데이터 베이스 사용자의 접속 비밀번호 입력
+        String password = "rlacodms01";
 
         try {
             Class.forName(driver);
