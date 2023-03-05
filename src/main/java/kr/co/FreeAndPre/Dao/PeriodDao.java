cf63ce4833db37d909997b965d7d481ad72a4a36
kche1106 <kche011106@gmail.com>
2023-03-05 10:36:02 +0900

    add validation for period cycles over 50 days

--- src/main/java/kr/co/FreeAndPre/Dao/PeriodDao.java
+++ src/main/java/kr/co/FreeAndPre/Dao/PeriodDao.java
@@ -100,7 +100,6 @@ public class PeriodDao {
      */
     public int insertPeriod(PeriodDto periodDto) {
         PreparedStatement pstmt = null;
-        Statement stmt = null;
         Connection con = null;
 
         try {
@@ -116,17 +115,19 @@ public class PeriodDao {
             int res = pstmt.executeUpdate();
 
             /* User 테이블의 주기(last_cycle), 기간(term) 업데이트 */
-            stmt = con.createStatement();
-
             //최근 4개월 월경 시작일 가져오기
-            ResultSet rs = stmt.executeQuery("SELECT start_date FROM period ORDER BY end_date DESC LIMIT 4;");
+            pstmt = con.prepareStatement("SELECT start_date FROM period WHERE email = ? ORDER BY end_date DESC LIMIT 4;");
+            pstmt.setString(1, periodDto.getEmail());
+            ResultSet rs = pstmt.executeQuery();
             List<String> start_date = new ArrayList<>();
             while(rs.next()) {
                 start_date.add(rs.getString(1));
             }
 
             //최근 4개월 월경 마지막일 가져오기
-            rs = stmt.executeQuery("SELECT end_date FROM period ORDER BY end_date DESC LIMIT 4;");
+            pstmt = con.prepareStatement("SELECT end_date FROM period WHERE email = ? ORDER BY end_date DESC LIMIT 4;");
+            pstmt.setString(1, periodDto.getEmail());
+            rs = pstmt.executeQuery();
             List<String> end_date = new ArrayList<>();
             while(rs.next()) {
                 end_date.add(rs.getString(1));
@@ -138,9 +139,15 @@ public class PeriodDao {
 
             List<Long> cycle_gap = new ArrayList<>();  //각 달의 월경 주기
             for(int i = 0; i < start_date.size() - 1; i++) {  //(새로 입력한 월경 시작일 - 저번 달의 월경 시작일)
-                cycle_gap.add(i, (new SimpleDateFormat("yyyy-MM-dd").parse(start_date.get(i)).getTime() -
-                        new SimpleDateFormat("yyyy-MM-dd").parse(start_date.get(i + 1)).getTime()) / 1000 / (24*60*60));
+
+                Long gap = (new SimpleDateFormat("yyyy-MM-dd").parse(start_date.get(i)).getTime() -
+                        new SimpleDateFormat("yyyy-MM-dd").parse(start_date.get(i + 1)).getTime()) / 1000 / (24*60*60);
+                if(gap >= 50)
+                    cycle_gap.add(i, Long.valueOf(28));
+                else
+                    cycle_gap.add(i, gap);
             }
+
             long cycle = 0, cycle_sum = 0;
             for(int i = 0; i < cycle_gap.size(); i++) {
                 cycle_sum += cycle_gap.get(i);
